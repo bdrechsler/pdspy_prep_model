fdccfcc4cad99c3002375d32700047d8a1496083
Blake Drechsler <wbd814@gmail.com>
2024-07-17 10:24:09 -0400

    added intial files

---
+++ create_config.py
@@ -0,0 +1,142 @@
+import os
+import glob
+import numpy as np
+
+def write_dict(d, d_name, file):
+        file.write(d_name + ' = {\n')
+        for key, val in d.items():
+            file.write('        "{0}":{1},\n'.format(key, val).replace('array', 'np.array'))
+        file.write('        }\n\n')
+
+def write_dynesty_params(file, nwalkers=100, steps_per_iter=20, max_nsteps=2000,
+                        nplot=5, nlive_init=250, nlive_batch=250, maxbatch=0,
+                        dlogz=0.05, walks=25):
+    
+    file.write('nwalkers = {}\n'.format(nwalkers))
+    file.write('steps_per_iter = {}\n'.format(steps_per_iter))
+    file.write('max_nsteps = {}\n'.format(max_nsteps))
+    file.write('nplot = {}\n'.format(nplot))
+    file.write('nlive_init = {}\n'.format(nlive_init))
+    file.write('nlive_batch = {}\n'.format(nlive_batch))
+    file.write('maxbatch = {}\n'.format(maxbatch))
+    file.write('dlogz = {}\n'.format(dlogz))
+    file.write('walks = {}\n\n'.format(walks))
+
+
+def create_config(data_path, model_path, line, disk_type, dpc):
+    data_file = glob.glob(data_path + "*.hdf5")[0]
+    image_file = glob.glob(data_path + "*.image.fits")[0]
+
+    line_dict = {'C18O': '219.56035410GHz',
+                '13CO': '220.39868420GHz',
+                'CH3OH': '241.80652400GHz',
+                'C17O': '224.714743800GHz'}
+
+    visibilities = {
+            "file":[data_file],
+            "binsize":[8057.218995847603],
+            "pixelsize":[0.01],
+            "freq":[line_dict[line]],
+            "lam":[line],
+            "npix":[1024],
+            "x0":[0.0],
+            "y0":[0.0],
+            "gridsize":[1024],
+            "weight":[0.25],
+            # Info for the image.
+            "image_file":[image_file],
+            "image_pixelsize":[0.025],
+            "image_npix":[512],
+            # Info for the plots.
+            "nrows":[7],
+            "ncols":[8],
+            "ind0":[1],
+            "fmt":['5.2f'],
+            "ticks":[np.array([-250,-200,-100,0,100,200,250])],
+            "image_ticks":[np.array([-0.75,-0.5,0,0.5,0.75])],
+            "nphi":[256],
+            "nr":[2],
+            }
+
+    spectra = {
+            "file":[],
+            "bin?":[],
+            "nbins":[],        
+            "weight":[],
+            }
+
+
+    images = {
+            "file":[],
+            "npix":[],
+            "pixelsize":[],
+            "lam":[],
+            "bmaj":[],
+            "bmin":[],
+            "bpa":[],
+            "ticks":[],
+            "plot_mode":[],
+            }
+
+
+    parameters = {
+            # Stellar parameters.
+            "logM_star":{"fixed":False, "value":-0.3, "limits":[-1.5,1.0]},
+            # Disk parameters.
+            "disk_type":{"fixed":True, "value":disk_type, "limits":[0.,0.]},
+            "logM_disk":{"fixed":False, "value":-3.0, "limits":[-10.,-2.5]},
+            "logR_disk":{"fixed":False, "value":2, "limits":[1.,2.6]},
+            "gamma":{"fixed":False, "value":1.0, "limits":[0.0,2.0]},
+            "f_M_large":{"fixed":True, "value":0.8, "limits":[0.05, 1.0]},
+            # Disk temperature parameters.
+            "logT0":{"fixed":False, "value":2.5, "limits":[1.,3.0]},
+            "q":{"fixed":True, "value":0.25, "limits":[0.,1.]},
+            "loga_turb":{"fixed":True, "value":-3.0, "limits":[-3.,-0.875]},
+            # Envelope parameters.
+            "envelope_type":{"fixed":True, "value":"ulrich-tapered", "limits":[0.,0.]},
+            "logM_env":{"fixed":False, "value":-3., "limits":[-6., -2.]},
+            "logR_env":{"fixed":False, "value":4., "limits": [2.,4.]},
+            "loga_turb_env":{"fixed":True, "value":-3.0, "limits":[-3.,-0.875]},
+            # Gas parameters.
+            "gas_file1":{"fixed":True, "value":"c18o.dat", "limits":[0.,0.]},
+            "logabundance1":{"fixed":True, "value":np.log10(1.0e-4/560.), "limits":[-10.,-2.]},
+        # Viewing parameters.
+            "i":{"fixed":False, "value":90.0, "limits":[0.,180.]},
+            "pa":{"fixed":False, "value":94.85, "limits":[0.0, 360.0]},
+            "x0":{"fixed":False, "value":0, "limits":[-0.3,0.3]},
+            "y0":{"fixed":False, "value":0, "limits":[-0.3,0.3]},
+            "dpc":{"fixed":True, "value":dpc, "limits":[1.,1e6]},
+            "Ak":{"fixed":True, "value":0., "limits":[0.,1.]},
+            "v_sys":{"fixed":False, "value":6.0, "limits":[0.0, 10.0]}
+            }
+
+    if "dartois" in disk_type:
+            del parameters["logT0"]
+            del parameters["q"]
+            parameters["logTmid0"] = {"fixed":False, "value":2.5, "limits":[0.,3.0]}
+            parameters["logTatm0"] = {"fixed":False, "value":2.5, "limits":[1.,3.5]}
+            parameters["pltgas_mid"] = {"fixed":False, "value":0.5, "limits":[0.,1.]}
+            parameters["pltgas_atm"] = {"fixed":False, "value":"pltgas_mid", "limits":[0.,1.]}
+            parameters["zq0"] = {"fixed":False, "value":0.1, "limits":[0.01,0.5]}
+
+    with open(model_path + 'config.py', 'w') as config:
+        # write import statements
+        config.write('import numpy as np\n')
+        config.write('import os\n\n')
+        
+        # write visibility dictionary
+        write_dict(visibilities, 'visibilities', config)
+
+        # write spectra dictionary
+        write_dict(spectra, 'spectra', config)
+
+        # write images dictionary
+        write_dict(images, 'images', config)
+
+        # write dynesty params
+        write_dynesty_params(config)
+
+        # write model params
+        write_dict(parameters, 'parameters', config)
+
+
---
+++ prep_lines.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import numpy as np
+from casatasks import mstransform, tclean, listobs, concat, rmtables, impbcor, exportfits
+import pdspy.interferometry as uv
+import argparse
+from create_config import create_config
+
+
+"""###################################
+###### Regridding Parameters #########
+######################################"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--source', type=str)
+parser.add_argument('-w', '--width', type=str, default='0.334km/s')
+parser.add_argument('-n', '--nchan', type=int, default=36)
+parser.add_argument('-v', '--svel', type=str, default='0.0km/s')
+parser.add_argument('-l', '--line', type=str, default='C18O')
+parser.add_argument('-uv', '--uvcut', action='store_false')
+parser.add_argument('-r', '--robust', type=float, default=2.0)
+parser.add_argument('-p', '--data_path', type=str, default='./data/')
+parser.add_argument('-m', '--model_path', type=str)
+parser.add_argument('-dt', '--disk_type', type=str, default='dartois-truncated')
+parser.add_argument('-d', '--dpc', type=float, default=132.)
+parser.add_argument('-c', '--just_config', action='store_true')
+args = parser.parse_args()
+
+line_dict = {'C18O': '219.56035410GHz',
+             '13CO': '220.39868420GHz',
+             'CH3OH': '241.80652400GHz',
+             'C17O': '224.714743800GHz'}
+
+source = args.source # source name
+wid = args.width # channel width
+nch = args.nchan # number of channels
+svel = args.svel # starting velocity
+linename = args.line
+rfreq = line_dict[linename] # rest freq of line
+parallel = False
+
+# path to data
+data_dir = args.data_path
+line_vis_list = glob.glob(data_dir + "*spectral_line.ms") # list of spectral line ms files
+
+"""
+#### Regrid the data #####
+"""
+if not args.just_config:
+    regrid_vis = []
+
+    for line_vis in line_vis_list:
+        # find spectral window corresponding to rfreq
+
+        # create listobs file
+        listfile = line_vis.replace('.ms', '.listobs') 
+        os.system("rm -rf " + listfile)
+        listobs(vis=line_vis, listfile=listfile, verbose=False)
+    
+        # read listobs file into a list of lines
+        listobs_file = open(listfile, "r")
+        lines = listobs_file.readlines()
+
+        # find lines corresponding to spws
+        for i in range(len(lines)):
+            split_line = lines[i].split() # split line into list of strings
+            if split_line: # make sure line is not empty
+                if split_line[0] == 'SpwID':
+                    first_ind = i+1
+                elif split_line[0] == 'Antennas:':
+                    last_ind = i
+
+        spw_header = lines[first_ind - 1] # line defining spw columns
+        spw_lines = lines[first_ind:last_ind] # lines correponding to the spws
+
+        # find spw with central frequency closest to rfreq
+        ctrfreq_ind = spw_header.split().index('CtrFreq(MHz)')
+        spws = [spw_lines[i].split()[ctrfreq_ind] for i in range(len(spw_lines))]
+        spws_array = np.array(spws).astype('float64')
+        freq_MHz = float(rfreq[:-3]) * 1000
+        spw = str(np.argmin(np.abs(spws_array-freq_MHz)))
+        listobs_file.close()
+
+        outfile = line_vis.replace('spectral_line.ms', linename+'_lsrk.ms') # name of output ms file
+        print("Creating " + outfile)
+        os.system("rm -rf " + outfile)
+
+        mstransform(vis=line_vis, regridms=True, mode='velocity', outputvis=outfile,
+                    outframe='LSRK', veltype='radio', restfreq=rfreq, datacolumn='data',
+                    width=wid, nchan=nch, start=svel, combinespws=False, keepflags=False,
+                    timeaverage=True, timebin='30.25s', spw=spw)
+        
+        regrid_vis.append(outfile)
+        
+    """
+    ##### Clean and Image the Data #######
+    """
+
+    imagename = data_dir + source + '_' + linename + '_t2000klam'
+    for ext in ['.image','.mask','.model','.pb','.psf','.residual','.sumwt','.workingdirectory']:
+        os.system('rm -rf ' + imagename + ext)
+
+    print("Running tlean:")
+
+    if args.uvcut:
+        uvrange='>50klambda'
+    else:
+        uvrange='all'
+
+    tclean(vis=regrid_vis, spw='', imagename=imagename, specmode='cube', imsize=512,
+        deconvolver='hogbom', start=svel, width=wid, nchan=nch, outframe='LSRK',
+        veltype='radio', restfreq=rfreq, cell='0.025arcsec',
+        gain=0.1, niter=20000, weighting='briggs', robust=args.robust, threshold='1.0mJy',
+        usemask='auto-multithresh', sidelobethreshold=2.0, noisethreshold=4.0,
+        lownoisethreshold=1.0, interactive=False, restoringbeam='common',
+        uvtaper=['2000klambda'], uvrange=uvrange, parallel=parallel)
+
+    # primary beam correction
+    myimages = glob.glob(data_dir + "*.image")
+
+    rmtables(data_dir + '*.pbcor')
+    for image in myimages:
+        impbcor(imagename=image, pbimage=image.replace('.image','.pb'), outfile=image.replace('.image', '.pbcor'))
+
+    # export the images
+    myimages = glob.glob(data_dir + "*.image")
+    for image in myimages:
+        exportfits(imagename=image, fitsimage=image+'.fits', overwrite=True)
+
+    myimages = glob.glob(data_dir + '*.pbcor')
+    for image in myimages:
+        exportfits(imagename=image, fitsimage=image+'.fits', overwrite=True)
+
+    myimages = glob.glob(data_dir + "*.pb")
+    for image in myimages:
+        exportfits(imagename=image, fitsimage=image+'.fits', overwrite=True)
+
+    for ext in ['*.pb', '*.psf', '*.model', '*.residual', '*.mask', '*.image', '*.pbcor']:
+        os.system("rm -rf " + data_dir + ext)
+
+    """
+    ###### Write Data to an HDF5 file #######
+    """
+
+    # concatinate regridded visibility files
+    os.system("rm -rf " + data_dir + "*concat.ms")
+    concat_file = data_dir + source + '_' + linename + '_' + 'concat.ms'
+    concat(vis=regrid_vis, concatvis=concat_file)
+
+    # load concated ms file into pdspy
+    data = uv.readms(filename=concat_file, datacolumn='data')
+
+    # find indicies where weights are non-zero
+    if args.uvcut:
+        good, = np.where((data.weights[:,0] > 0) & (data.uvdist > 50000))
+        cut_name = "_50klam"
+    else:
+        good, = np.where(data.weights[:,0] > 0)
+        cut_name = ""
+
+    # find data values at these indicies
+    new_u = data.u[good]
+    new_v = data.v[good]
+    new_real = data.real[good,:]
+    new_imag = data.imag[good,:]
+    new_weights = data.weights[good,:]
+    # create hdf5 file
+    os.system("rm -rf *.hdf5")
+    output_file = data_dir + source + '_' + linename + cut_name +  '.hdf5'
+    new_data = uv.Visibilities(new_u, new_v, data.freq, new_real, new_imag, new_weights)
+    new_data.write(output_file)
+
+if not os.path.exists(args.model_path):
+    os.system("mkdir {}".format(args.model_path))
+create_config(data_dir, args.model_path, linename, args.disk_type, args.dpc)
+
+
+    
+
